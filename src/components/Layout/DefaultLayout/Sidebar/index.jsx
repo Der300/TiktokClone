c1942ee18b90bb890a8e8caab12cc108c0a0f364
Tran Van Vu Luan <tranvanvuluantp@gmail.com>
2025-02-27 21:48:13 +0700

    Build Frame Default Layout

--- src/components/Layout/DefaultLayout/Sidebar/index.jsx
+++ src/components/Layout/DefaultLayout/Sidebar/index.jsx
@@ -1,8 +1,14 @@
+import classNames from "classnames/bind";
+
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 const Sidebar = () => {
     return ( 
-        <>
-        <h2>Sidebar</h2>
-        </>
+        <aside className={cx('wrapper')}>
+            <h2>side bar</h2>
+        </aside>
      );
 }
  
